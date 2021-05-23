ee3ad751b6d5eee5ad16df797bca5fb48bab7c1e
AhmetGH <ahmetgocmen07@gmail.com>
2021-05-23 21:31:31 +0300

    sekizinci gonderme

--- goruntu.php
+++ goruntu.php
@@ -1,3 +1,31 @@
 <?php
+  error_reporting(0);
   
-?>
+  $adi=$_POST["Ad"];
+  $soyadi=$_POST["Soyad"];
+  $emaili=$_POST["email"];
+  $Dogumtarihi=$_POST["Dogumtarih"];
+  $Cinsiyeti1=$POST["Erkek"];
+  $Cinsiyeti2=$POST["Kadın"];
+  $Cinsiyeti3=$POST["Belirsiz"];
+  $option1=$_POST["idareeder"];
+  $option1=$_POST["Gayetguzel"];
+  $option1=$_POST["Kotu"];
+  $option1=$_POST["bilemedim"];
+  $gelengonderi=$_POST["gonderme"];
+
+  echo "<b>adiniz:</b>$adi";
+  echo "<br></br>";
+  echo "<b>soyadiniz::</b>$soyadi";
+  echo "<br></br>";
+  echo "<b>e-maliniz:</b>$emaili";
+  echo "<br></br>";
+  echo "<b>Dogum tarihiniz:</b>$Dogumtarihi";
+  echo "<br></br>";
+  echo "<b>Cinsiyetiniz:</b>$Cinsiyeti1".$Cinsiyeti2.$Cinsiyeti3;
+  echo "<br></br>";
+  echo "<b>seçiminiz:</b>$option1";
+  echo "<br></br>";
+  echo "<b>öneri gönderiniz:</b>$gelengonderi";
+
+  ?>
